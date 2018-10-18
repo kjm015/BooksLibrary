0000266390f0eec1dd26ea40bff491dc69f49193
Kevin Miyata <kjm015@yahoo.com>
2018-10-18 09:34:29 -0500

    Initial Commit

---
+++ BooksLibrary/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  BooksLibrary
+//
+//  Created by Kevin Miyata on 10/18/18.
+//  Copyright © 2018 Kevin Miyata. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
